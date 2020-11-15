c3a5f9276f86ab99cf40c6fb43d2a284b6f344df
abeidahmed <abeidahmed92@gmail.com>
2020-11-15 19:42:45 +0530

    add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  before_create :generate_auth_token
+  before_save :normalize_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates_presence_of :email, :name
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_length_of :email, :name, maximum: 255
+  validates_format_of :email, with: VALID_EMAIL_REGEX
+  validates_length_of :password, minimum: 6, allow_blank: true
+
+  private
+  def generate_auth_token
+    generate_token(:auth_token)
+  end
+
+  def normalize_email
+    self.email = email.downcase
+  end
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  describe "validations" do
+    it { should have_secure_password }
+
+    it { should validate_presence_of(:email)}
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+    it { should_not allow_value("abeidmama", "abeidm@em@.com").for(:email) }
+
+    it { should allow_value("abeidmama@example.com", "abeidm@em.uk.com").for(:email) }
+
+    it { should validate_length_of(:email).is_at_most(255) }
+
+    it { should validate_presence_of(:name) }
+
+    it { should validate_length_of(:name).is_at_most(255) }
+
+    it "is should lowercase email before saving" do
+      subject.email = subject.email.upcase
+      subject.save!
+      expect(subject.reload.email).to eq(subject.email.downcase)
+    end
+
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe "before creating a new user" do
+    it "is should set auth_token with Secure Random hash" do
+      subject.auth_token = nil
+      subject.save!
+      expect(subject.reload.auth_token).to_not be_nil
+    end
+  end
 end
