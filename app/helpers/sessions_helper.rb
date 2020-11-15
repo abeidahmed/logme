3321da38fec7b724d7c1ff67ce054a121ff82bd8
abeidahmed <abeidahmed92@gmail.com>
2020-11-15 19:46:26 +0530

    add session helper methods

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,13 @@
+module SessionsHelper
+  def login(user)
+    cookies.permanent[:auth_token] = user.auth_token
+  end
+
+  def current_user
+    @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
+end
