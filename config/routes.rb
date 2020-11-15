0cdb5d0b3c4492f563a5ecd2a3fd3891bfda7588
abeidahmed <abeidahmed92@gmail.com>
2020-11-15 22:43:50 +0530

    make login flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: %i(new create)
-  resources :sessions, only: %i(create)
+  resources :sessions, only: %i(new create)
   root "static_pages#home"
 end
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.feature "Signins", type: :feature do
+  describe "with valid email and password" do
+    it "should login user" do
+      sign_in
+
+      expect(current_url).to eq(root_url)
+      expect(page).to have_content("Signed in")
+    end
+  end
+
+  describe "when the user is already logged in" do
+    it "should redirect to root_url" do
+      sign_in
+      visit new_session_url
+
+      expect(current_url).to eq(root_url)
+    end
+  end
+end
