e4020221e8ee3dd878ea169baf07b836f36669e7
abeidahmed <abeidahmed92@gmail.com>
2020-11-16 18:44:20 +0530

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+HqMembership.destroy_all
+ProjectMembership.destroy_all
+Project.destroy_all
+User.destroy_all
+Headquarter.destroy_all
 
 10.times do
-  user = User.create! name: Faker::FunnyName.two_word_name, email: Faker::Internet.unique.email, password: "mamakane"
-  headquarter = Headquarter.create! name: Faker::Company.name, description: Faker::Lorem.paragraph
+  headquarter = Headquarter.create!(name: Faker::Company.name, description: Faker::Lorem.paragraph)
 
-  role = %w(owner member)
+  8.times do
+    user = User.create!(name: Faker::FunnyName.two_word_name, email: Faker::Internet.unique.email, password: "mamakane")
+    role = %w(owner member)
+    hq_team = HeadquarterTeamAdder.new(headquarter: headquarter, user: user, role: role.sample)
+    hq_team.save
 
-  hq_team = HeadquarterTeamAdder.new(headquarter: headquarter, user: user, role: role.sample)
-  hq_team.save
-
-  project = Project.create! name: Faker::Company.name, url: Faker::Internet.url, subdomain: Faker::Internet.unique.domain_word, headquarter: headquarter
-  project_team = ProjectTeamAdder.new(project: project, user: user)
-  project_team.save
+    project = Project.create!(
+      name: Faker::Company.name,
+      url: Faker::Internet.url,
+      subdomain: Faker::Internet.unique.domain_word,
+      description: Faker::Lorem.paragraph,
+      headquarter: headquarter
+    )
+    project_team = ProjectTeamAdder.new(project: project, user: user)
+    project_team.save
+  end
 end
