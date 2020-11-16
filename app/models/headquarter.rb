27af8aad7ac6f78707d444622ac414b435f8f4db
abeidahmed <abeidahmed92@gmail.com>
2020-11-16 11:45:24 +0530

    add hq model validations

--- app/models/headquarter.rb
+++ app/models/headquarter.rb
@@ -1,2 +1,5 @@
 class Headquarter < ApplicationRecord
+  validates_presence_of :name
+  validates_length_of :name, maximum: 255
+  validates_length_of :description, maximum: 500
 end
--- spec/models/headquarter_spec.rb
+++ spec/models/headquarter_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Headquarter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+
+    it { should validate_length_of(:name).is_at_most(255) }
+
+    it { should validate_length_of(:description).is_at_most(500) }
+  end
 end
