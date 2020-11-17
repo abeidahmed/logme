582f089c8252109e5aef4a80246d204bde633254
abeidahmed <abeidahmed92@gmail.com>
2020-11-17 21:32:25 +0530

    refactor active-link controller

--- app/javascript/controllers/active_link_controller.js
+++ app/javascript/controllers/active_link_controller.js
@@ -8,21 +8,22 @@ export default class extends Controller {
       const href = el.getAttribute('href');
 
       if (this.dataExact) {
-        if (
-          location.href === href ||
-          location.pathname + location.search === href
-        ) {
+        if (location.href === href || this.urlLocation === href) {
           el.classList.add(this.activeClass);
         }
       } else {
         if (
           location.href.startsWith(href) ||
-          (location.pathname + location.search).startsWith(href)
+          this.urlLocation.startsWith(href)
         ) {
           el.classList.add(this.activeClass);
         }
       }
     });
+  }
+
+  get urlLocation() {
+    return location.pathname + location.search;
   }
 
   get activeClass() {
