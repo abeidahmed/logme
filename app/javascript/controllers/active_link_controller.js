98b04686a36d6e4aa4954d9246e9651ee8f235f6
abeidahmed <abeidahmed92@gmail.com>
2020-11-17 14:46:51 +0530

    add active link stimulus controller

---
+++ app/javascript/controllers/active_link_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+  static targets = ['link'];
+
+  initialize() {
+    this.linkTargets.forEach((el) => {
+      const href = el.getAttribute('href');
+
+      if (this.dataExact) {
+        if (location.href === href) {
+          el.classList.add(this.activeClass);
+        }
+      } else {
+        if (location.href.startsWith(href)) {
+          el.classList.add(this.activeClass);
+        }
+      }
+    });
+  }
+
+  get activeClass() {
+    return this.data.get('class');
+  }
+
+  get dataExact() {
+    return JSON.parse(this.data.get('exact'));
+  }
+}
