b49a80a1650136bcb24ccf5ff385c7e5edde0048
abeidahmed <abeidahmed92@gmail.com>
2020-11-18 15:06:40 +0530

    register password reset factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,15 @@ FactoryBot.define do
     sequence(:email) { |n| "johndoe#{n}@example.com" }
     password { "passwordsecret" }
     auth_token { "helloworld" }
+
+    trait :forgot_password do
+      password_reset_token { SecureRandom.urlsafe_base64 }
+      password_reset_sent_at { Time.zone.now }
+    end
+
+    trait :with_expired_token do
+      forgot_password
+      password_reset_sent_at { 2.hours.ago }
+    end
   end
 end
