d44fd337780d6204398d8372b38d4561b2328eba
abeidahmed <abeidahmed92@gmail.com>
2020-11-19 10:01:56 +0530

    fix mailer bug

--- app/mailers/hq_membership_mailer.rb
+++ app/mailers/hq_membership_mailer.rb
@@ -1,8 +1,8 @@
 class HqMembershipMailer < ApplicationMailer
-  def invite(user, headquarter, new_user:)
+  def invite(user, headquarter, **options)
     @user         = user
     @headquarter  = headquarter
-    @new_user     = new_user
+    @new_user     = options[:new_user]
 
     mail to: user.email, subject: "You have an invitation to #{headquarter.name}"
   end
