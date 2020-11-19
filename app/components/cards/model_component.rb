ba3dea97d1ad13e0ae64449b904c25d60b4a118d
abeidahmed <abeidahmed92@gmail.com>
2020-11-19 15:17:18 +0530

    add pending option to model card component

--- app/components/cards/model_component.rb
+++ app/components/cards/model_component.rb
@@ -1,8 +1,9 @@
 class Cards::ModelComponent < ApplicationComponent
   with_content_areas :body, :actions
 
-  def initialize(title:, url:)
-    @title  = title
-    @url    = url
+  def initialize(title:, url:, pending:)
+    @title    = title
+    @url      = url
+    @pending  = pending
   end
 end
--- app/models/concerns/user/project_tenant.rb
+++ app/models/concerns/user/project_tenant.rb
@@ -5,7 +5,15 @@ class User
     end
 
     def project_invite_accepted?(project)
-      project_memberships.find_by(project_id: project.id).invitation_accepted?
+      find_project_membership(project).invitation_accepted?
+    end
+
+    def project_invite_pending?(project)
+      !find_project_membership(project).invitation_accepted?
+    end
+
+    def find_project_membership(project)
+      project_memberships.find_by(project_id: project.id)
     end
   end
 end
